fa6b86356aa50c7fc92cd3354db71f90106ae717
IamEndo <endo@nexa.org>
2025-11-05 21:33:05 +0200

    Update textarea.tsx

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -13,7 +13,7 @@ export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       <textarea
         ref={ref}
         className={cn(
-          "w-full rounded-xl border border-slate-300 dark:border-slate-700 bg-white dark:bg-slate-900 px-3 py-2 text-sm outline-none placeholder:text-slate-400 dark:placeholder:text-slate-500 focus-visible:ring-2 focus-visible:ring-slate-400 dark:focus-visible:ring-slate-700",
+          "w-full rounded-xl border border-slate-300 dark:border-neutral-800 bg-white dark:bg-black px-3 py-2 text-sm text-slate-900 dark:text-gray-100 outline-none placeholder:text-slate-400 dark:placeholder:text-gray-500 focus-visible:ring-2 focus-visible:ring-slate-400 dark:focus-visible:ring-neutral-700",
           className
         )}
         {...props}
